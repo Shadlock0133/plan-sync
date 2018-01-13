e6eb772c17945306cf63ccb18cd075b4b8938517
Aurora <tomek-kubel2@wp.pl>
2018-01-13 22:18:35 +0100

    add opening of new plan in browser + some refactor

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 extern crate app_dirs;
 extern crate reqwest;
 extern crate chrono;
+// extern crate clap;
+// extern crate failure;
 
 use app_dirs::*;
 use chrono::prelude::*;
@@ -8,12 +10,35 @@ use chrono::prelude::*;
 use std::error::Error;
 use std::fs::File;
 use std::io::{Read, Write};
+use std::process::Command;
 
 const URL: &'static str = "http://plany.wel.wat.edu.pl/lato/E7Q2S1.htm";
 const APP_INFO: AppInfo = AppInfo { name: "plan-sync", author: "Shadlock0133" }; 
+const BROWSER: &'static str = "C:\\Program Files\\Mozilla Firefox\\firefox.exe";
+
+enum AppError {
+    NetworkError,
+    FileWriteError,
+}
 
 fn main() {
-    let new_plan = get_new_plan().expect("Network error");
+    match update() {
+        Ok(_) | Err(AppError::NetworkError) => open(),
+        _ => println!("FIX ME"),
+    }
+}
+
+fn open() {
+    let path = app_root(AppDataType::UserData, &APP_INFO).unwrap()
+        .join(get_plan_filename(&get_cached_timestamp().unwrap()));
+    Command::new(BROWSER)
+        .arg(path)
+        .spawn()
+        .expect("open() failed");
+}
+
+fn update() -> Result<(), AppError> {
+    let new_plan = get_new_plan().map_err(|_| AppError::NetworkError)?;
     let cached_plan = get_cached_timestamp()
         .and_then(|timestamp| {
             get_cached_plan(&timestamp)
@@ -22,7 +47,7 @@ fn main() {
         Ok(cached_plan) => {
             if &cached_plan == &new_plan {
                 println!("Files equal, nothing to do.");
-                return;
+                return Ok(());
             } else {
                 println!("Files not equal, caching new.");
             }
@@ -31,7 +56,8 @@ fn main() {
             println!("No old timestamp, creating new");
         }
     }
-    save_new_plan(&new_plan).expect("File saving error");
+    save_new_plan(&new_plan).map_err(|_| AppError::FileWriteError)?;
+    Ok(())
 }
 
 fn get_cached_timestamp() -> Result<String, Box<Error>> {
@@ -42,8 +68,12 @@ fn get_cached_timestamp() -> Result<String, Box<Error>> {
     Ok(buf)
 }
 
+fn get_plan_filename(timestamp: &str) -> String {
+    format!("plan_{}.html", timestamp)
+}
+
 fn get_cached_plan(timestamp: &str) -> Result<Vec<u8>, Box<Error>> {
-    let filename = format!("plan_{}.html", timestamp);
+    let filename = get_plan_filename(timestamp);
     let path = app_root(AppDataType::UserData, &APP_INFO)?.join(filename);
     let mut file = File::open(path)?;
     let mut buf: Vec<u8> = Vec::new();
@@ -67,7 +97,7 @@ fn get_new_plan() -> Result<Vec<u8>, Box<Error>> {
 fn save_new_plan(buf: &[u8]) -> Result<(), Box<Error>> {
     let timestamp = get_current_timestamp()?;
 
-    let filename = format!("plan_{}.html", timestamp);
+    let filename = get_plan_filename(&timestamp);
     let path = app_root(AppDataType::UserData, &APP_INFO)?.join(filename);
     let print_path = path.to_string_lossy().to_string();
     let mut file = File::create(path)?;
